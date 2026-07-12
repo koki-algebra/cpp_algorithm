092c072a47c3bc7d7af58af4785cdfe9f55c622c
koki-algebra <biz.kokiyamada1022@gmail.com>
2026-07-12 22:41:50 +0900

    add abc085

---
+++ abc/abc085/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n;
+    cin >> n;
+    set<ll> s;
+    rep(i, n) {
+        ll d;
+        cin >> d;
+        s.insert(d);
+    }
+    cout << s.size() << "\n";
+
+    return 0;
+}
