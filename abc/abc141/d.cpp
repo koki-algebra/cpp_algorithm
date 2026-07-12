35e3948a7563311af6a256750c08a2bc5c9c2ddf
koki-algebra <biz.kokiyamada1022@gmail.com>
2026-07-12 23:52:07 +0900

    add abc141

---
+++ abc/abc141/d.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n, m;
+    cin >> n >> m;
+
+    vl a(n);
+    rep(i, n) cin >> a[i];
+    priority_queue<ll> pq(all(a));
+
+    rep(i, m) {
+        auto x = pq.top() / 2;
+        pq.pop();
+        pq.push(x);
+    }
+    ll ans = 0;
+    while (!pq.empty()) {
+        ans += pq.top();
+        pq.pop();
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
