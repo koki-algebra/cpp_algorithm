c437a7273dae143bdfbb47950089bd1bf2d5ce66
koki-algebra <biz.kokiyamada1022@gmail.com>
2026-07-12 22:53:12 +0900

    add abc241

---
+++ abc/abc241/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n, m;
+    cin >> n >> m;
+    map<ll, ll> p;
+    rep(i, n) {
+        ll a;
+        cin >> a;
+        p[a]++;
+    }
+    rep(i, m) {
+        ll b;
+        cin >> b;
+        if (!p[b]) {
+            cout << "No" << "\n";
+            return 0;
+        }
+        p[b]--;
+    }
+    cout << "Yes" << "\n";
+
+    return 0;
+}
