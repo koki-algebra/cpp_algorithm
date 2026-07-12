de85a4b6b5438b5fbbbe67810771ee8e30474d7c
koki-algebra <biz.kokiyamada1022@gmail.com>
2026-07-12 17:39:39 +0900

    abc099/a

--- abc/abc099/a.cpp
+++ abc/abc099/a.cpp
@@ -19,7 +19,14 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    cin >> n;
 
+    if (n < 1000) {
+        cout << "ABC" << "\n";
+    } else {
+        cout << "ABD" << "\n";
+    }
 
     return 0;
 }
