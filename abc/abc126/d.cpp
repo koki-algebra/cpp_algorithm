f4c7db0b86f5c5a0860cf63140095aed8e72d0a1
koki-algebra <biz.kokiyamada1022@gmail.com>
2026-07-21 00:41:24 +0900

    add abc126 d

--- abc/abc126/d.cpp
+++ abc/abc126/d.cpp
@@ -19,7 +19,36 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    ll n;
+    cin >> n;
+    vector<vector<pll>> graph(n);
+    rep(i, n - 1) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        u--, v--;
+        graph[u].push_back(make_pair(v, w));
+        graph[v].push_back(make_pair(u, w));
+    }
 
+    vl ans(n, 0);
+    stack<tuple<ll, ll, ll>> st;
+    st.push(make_tuple(0, -1, 1));
+    while (!st.empty()) {
+        auto [v, p, c] = st.top();
+        st.pop();
+        ans[v] = c;
+
+        for (auto e : graph[v]) {
+            if (e.first == p) continue;
+            if (e.second & 1) {
+                st.push(make_tuple(e.first, v, 1-c));
+            } else {
+                st.push(make_tuple(e.first, v, c));
+            }
+        }
+    }
+
+    rep(i, n) cout << ans[i] << "\n";
 
     return 0;
 }
