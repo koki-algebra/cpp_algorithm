731a251571887ea876c201d531dbf8a0bbd45870
koki-algebra <biz.kokiyamada1022@gmail.com>
2026-07-26 23:48:53 +0900

    add abc133 d

--- abc/abc133/d.cpp
+++ abc/abc133/d.cpp
@@ -19,7 +19,32 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    ll n;
+    cin >> n;
+    vl a(n);
+    rep(i, n) cin >> a[i];
 
+    ll sum2b1 = 0;
+    rep(i, n) {
+        if (i % 2 == 0) {
+            sum2b1 += a[i];
+        } else {
+            sum2b1 -= a[i];
+        }
+    }
+    ll b1 = sum2b1 / 2;
+
+    vl b(n);
+    b[0] = b1;
+    rep2(i, 1, n) b[i] = a[i - 1] - b[i - 1];
+    rep(i, n) {
+        cout << 2 * b[i];
+        if (i == n - 1) {
+            cout << "\n";
+        } else {
+            cout << " ";
+        }
+    }
 
     return 0;
 }
